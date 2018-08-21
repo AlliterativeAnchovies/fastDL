7de0d957472e3c4ebd8e1198fc4f645830083000
AlliterativeAnchovies <33570983+AlliterativeAnchovies@users.noreply.github.com>
2018-08-20 19:04:47 -0700

    Update fastDL.py

--- fastDL.py
+++ fastDL.py
@@ -34,14 +34,15 @@ def bashCP(cpFrom,cpTo):
     copier.copy(cpFrom,cpTo)"""
   copytree(cpFrom,cpTo)
 
+#https://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
 def copytree(src, dst, symlinks=False, ignore=None):
   for item in os.listdir(src):
     s = os.path.join(src, item)
     d = os.path.join(dst, item)
     if os.path.isdir(s):
-      shutil.copytree(s, d, symlinks, ignore)
+      copier.copytree(s, d, symlinks, ignore)
     else:
-      shutil.copy2(s, d)
+      copier.copy2(s, d)
 	
 def bash(inputs):
   return subprocess.check_output(inputs,shell=True)
